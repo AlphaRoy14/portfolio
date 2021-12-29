99534d4e53dcf06d14eee09793cb005f1cefe451
AlphaRoy14 <AlphaRoy14@gmail.com>
2021-12-29 00:29:06 -0800

    Update site url

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   siteShortTitle: "A. ", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "A Roy",
-  siteUrl: "arindaam.com",
+  siteUrl: "https://arindaam.com",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
   seoTitleSuffix: "A Roy", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
